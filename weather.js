331374518a8575ea42a55cc87c2a571af217035c
SWLeee <tkddnjs0687@naver.com>
2020-08-23 23:55:53 +0900

    #3.9 Getting the Weather part Two API

--- weather.js
+++ weather.js
@@ -1,5 +1,24 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "2eb842bcedb830ed0d752c4c85d8b70c";
 const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      //   console.log(weather);
+      //   console.log(temperature);
+      //   console.log(place);
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -14,6 +33,7 @@ function handleGeoSuccess(position) {
     longitude, // longitude: longitude
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -23,11 +43,12 @@ function askForCoords() {
   navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
 }
 function loadCoords() {
-  const loadedCords = localStorage.getItem(COORDS);
-  if (loadedCords === null) {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
     askForCoords();
   } else {
-    // getWeather
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
   }
 }
 
